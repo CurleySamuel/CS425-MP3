3fefcf08ff2c577275f7502fea2d03607da585dd
Sam Curley <CurleySamuel@gmail.com>
2015-05-02 15:35:07 -0500

    backup

--- maekawa.py
+++ maekawa.py
@@ -2,8 +2,10 @@ import math
 import sys
 import Queue
 import threading
-# from termcolor import colored
+from termcolor import colored
 import time
+import random
+import copy
 
 
 N = 9
@@ -33,168 +35,172 @@ def main():
     # Initialize threads
     for x in range(1, N + 1):
         a = []
-        a.append(Queue.Queue())
+        a.append(Queue.PriorityQueue())
         a.append({})
         a.append(threading.Thread(target=main_thread_function, args=(x,)))
         threads.append(a)
 
     # Start threads
-    for x in range(1, N + 1):
+    shuffled = range(1, N + 1)
+    random.shuffle(shuffled)
+    for x in shuffled:
+        threads[x][1]['sem'] = threading.Semaphore()
+        threads[x][1]['vote'] = None
+        threads[x][1]['nodes'] = []
         threads[x][2].daemon = True
         threads[x][2].start()
     time.sleep(tot_exec_time)
 
 
 def main_thread_function(thread_id):
+    threads[thread_id][1]['state'] = "Idle"
+    threads[thread_id][1]['child'] = threading.Thread(
+        target=message_handler_threads, args=(thread_id,))
+    threads[thread_id][1]['child'].daemon = True
+    threads[thread_id][1]['child'].start()
     while 1:
-        request_critical(thread_id)
-        handle_messages(thread_id, request=True)
-        release_critical(thread_id)
-        handle_messages(thread_id, release=True)
+        r_time = random.random()  # time.time()
+        # print "{} - Requesting Critical".format(thread_id)
+        while 1:
+            threads[thread_id][1]['state'] = "Requesting"
+            threads[thread_id][1]['sem'] = threading.Semaphore()
+            request_critical(thread_id, r_time=r_time)
+            wait_for_critical(thread_id)
+            if threads[thread_id][1]['state'] is not "Failed":
+                break
+        print "{} - Acquired from {}".format(thread_id, " ".join([str(x) for x in threads[thread_id][1]['nodes']]))
+        threads[thread_id][1]['state'] = "Acquired"
+        time.sleep(cs_int / 1000.0)
+        print "{} - Released".format(thread_id)
+        release_critical(thread_id, r_time=r_time)
+        threads[thread_id][1]['state'] = "Idle"
+        time.sleep(next_req / 1000.0)
 
 
-def handle_messages(thread_id, request=False, release=False, critical=False):
-    if release:
-        end_time = time.time() + (next_req / 1000.0)
-    elif critical:
-        end_time = time.time() + (cs_int / 1000.0)
-    else:
-        end_time = time.time() + tot_exec_time
+def wait_for_critical(thread_id):
+    for x in range(0, 2 * n):
+        threads[thread_id][1]['sem'].acquire()
+        print "\t{} - +1!. State: {}. Nodes: {}\n".format(thread_id, threads[thread_id][1]['state'], " ".join([str(x) for x in threads[thread_id][1]['nodes']])),
 
-    granted = []
+
+def message_handler_threads(thread_id):
     while 1:
-        msg = busy_get(thread_id, end_time)
-        if msg is None:
-            print thread_id, "OUT CRITICAL"
-            return
-        if option:
-            print "{} {} {} {}\n".format(int(round(time.time() * 1000)),
-                                         thread_id, msg['src'], msg['action']),
-        if msg['action'] == "grant":
-            granted.append(msg['src'])
-            if len(granted) == 2 * n - 2:
-                # print "{} {} {}\n".format(int(round(time.time() * 1000)),
-                #  thread_id,
-                # ' '.join(map(str, granted))),
-                print thread_id, "IN CRITICAL"
-                return
-
-        elif msg['action'] == "request":
-            if threads[thread_id][1][0] is None:
-                send_yes_vote(thread_id, msg)
+        time, msg = threads[thread_id][0].get()
+        time = copy.copy(time)
+        msg = copy.copy(msg)
+        if msg['action'] is not "request" and msg['action'] is not "grant":
+            print "\t{} - Received {} from {}\n".format(thread_id, msg['action'], msg['src']),
+        if msg['action'] is "request":
+            if threads[thread_id][1]['vote'] is None:
+                send_grant_message(thread_id, msg)
             else:
-                if threads[thread_id][1][1] < msg['tstamp']:
-                    get_back_vote(thread_id, msg['src'], msg['tstamp'])
+                if threads[thread_id][1]['vote'][0] > msg['tstamp']:
+                    if threads[thread_id][1]['vote'][1] == msg['src']:
+                        send_grant_message(thread_id, msg)
+                    else:
+                        send_inquire_message(thread_id, msg)
                 else:
-                    send_no_vote(thread_id, msg)
-
-        elif msg['action'] == "release":
-            threads[thread_id][1][0] = None
-
-        elif msg['action'] == "deny":
-            pass
-
-        elif msg['action'] == "inquire":
-            # if msg['src'] in granted:
-            #    granted.remove(msg['src'])
-            send_yield(thread_id, msg)
-
-        elif msg['action'] == "yield":
+                    if threads[thread_id][1]['vote'][1] == msg['src'] and threads[thread_id][1]['vote'][0] == msg['tstamp']:
+                        send_grant_message(thread_id, msg)
+                    else:
+                        threads[thread_id][0].put((msg['tstamp'], msg))
+        elif msg['action'] is "grant":
+            if msg['src'] not in threads[thread_id][1]['nodes']:
+                threads[thread_id][1]['nodes'].append(msg['src'])
+                print "\t{}({}) - Received {} from {}. Votes: {}\n".format(thread_id, msg['tstamp'], msg['action'], msg['src'], " ".join([str(x) for x in threads[thread_id][1]['nodes']])),
+                threads[thread_id][1]['sem'].release()
+        elif msg['action'] is "release":
+            threads[thread_id][1]['vote'] = None
+        elif msg['action'] is "failed":
+            msg['action'] = 'request'
             msg['src'] = msg['alternative']
-            send_yes_vote(thread_id, msg)
-
-        elif msg['action'] == "no_yield":
-            msg['src'] = msg['alternative']
-            send_no_vote(thread_id, msg)
-
-
-def send_yes_vote(thread_id, msg):
-    threads[thread_id][1][0] = msg['src']
-    threads[thread_id][1][1] = msg['tstamp']
-    send_msg = {
-        "action": "grant",
-        "src": thread_id,
-        "tstamp": time.time()
-    }
-    threads[msg['src']][0].put(send_msg)
-
-
-def send_no_vote(thread_id, msg):
-    threads[thread_id][0].put(msg)
-    send_msg = {
-        "action": "deny",
-        "src": thread_id,
-        "tstamp": time.time()
-    }
-    threads[msg['src']][0].put(send_msg)
+            msg.pop('alternative')
+            threads[thread_id][0].put((msg['tstamp'], msg))
+        elif msg['action'] is "inquire":
+            state = threads[thread_id][1]['state']
+            if (state is "Requesting" or state is "Idle"):
+                threads[thread_id][1]['state'] = "Failed"
+                for x in range(2 * n):
+                    threads[thread_id][1]['sem'].release()
+                send_relinquish_message(thread_id, msg)
+            else:
+                send_failed_message(thread_id, msg)
+        elif msg['action'] is "relinquish":
+            if threads[thread_id][1]['vote'] is not None and threads[thread_id][1]['vote'][1] is msg['src']:
+                msg['src'] = msg['alternative']
+                msg['action'] = "grant"
+                msg.pop('alternative')
+                send_grant_message(thread_id, msg)
+            else:
+                print "\t{} - Old RELINQUISH received from {}. Alternative: {}.\n".format(thread_id, msg['src'], msg['alternative']),
+        else:
+            print colored("Unknown action '{}' received!", "red").format(msg['action'])
 
 
-def get_back_vote(thread_id, thread, tstamp):
-    send_msg = {
-        "action": "inquire",
-        "src": thread_id,
-        "alternative": thread,
-        "tstamp": tstamp
-    }
-    threads[threads[thread_id][1][0]][0].put(send_msg)
+def send_message(dst, msg):
+    threads[dst][0].put((msg['tstamp'], msg))
 
 
-def send_yield(thread_id, msg):
-    send_msg = {
-        "action": "yield",
-        "src": thread_id,
-        "alternative": msg['alternative'],
-        "tstamp": msg['tstamp']
-    }
-    threads[msg['src']][0].put(send_msg)
+# Reply to src.
+def send_grant_message(thread_id, imsg):
+    print "\t{} sending GRANT to {}\n".format(thread_id, imsg['src']),
+    threads[thread_id][1]['vote'] = (imsg['tstamp'], imsg['src'])
+    imsg['action'] = 'grant'
+    dst = imsg['src']
+    imsg['src'] = thread_id
+    send_message(dst, imsg)
 
 
-def send_no_yield(thread_id, msg):
-    send_msg = {
-        "action": "no_yield",
-        "src": thread_id,
-        "alternative": msg['alternative'],
-        "tstamp": msg['tstamp']
-    }
-    threads[msg['src']][0].put(send_msg)
+# Reply to src with original message.
+def send_failed_message(thread_id, imsg):
+    print "\t{} sending FAILED to {}. State: {}\n".format(thread_id, imsg['src'], threads[thread_id][1]['state']),
+    dst = imsg['src']
+    imsg['src'] = thread_id
+    imsg['action'] = 'failed'
+    send_message(dst, imsg)
 
 
-def busy_get(thread_id, end_time):
-    while time.time() < end_time:
-        try:
-            msg = threads[thread_id][0].get_nowait()
-            return msg
-        except Queue.Empty:
-            pass
-    return None
+# Reply to current voted node with same message, set alternative.
+def send_inquire_message(thread_id, imsg):
+    print "\t{} sending INQUIRE to {}. Alternative: {}\n".format(thread_id, threads[thread_id][1]['vote'][1], imsg['src']),
+    imsg['alternative'] = imsg['src']
+    imsg['src'] = thread_id
+    imsg['action'] = 'inquire'
+    send_message(threads[thread_id][1]['vote'][1], imsg)
 
 
-def request_critical(thread_id):
+# Reply to src with same message.
+def send_relinquish_message(thread_id, imsg):
+    dst = imsg['src']
+    imsg['src'] = thread_id
+    imsg['action'] = 'relinquish'
+    send_message(dst, imsg)
+
+
+def request_critical(thread_id, r_time=time.time()):
+    threads[thread_id][1]['nodes'] = []
     msg = {
         "action": "request",
         "src": thread_id,
-        "tstamp": time.time()
+        "tstamp": r_time
     }
-    try:
-        gen = voting_set(thread_id)
-        while 1:
-            voting_set_member = gen.next()
-            threads[voting_set_member][0].put(msg)
-    except StopIteration:
-        pass
+    send_to_voting_set(thread_id, msg)
 
 
-def release_critical(thread_id):
+def release_critical(thread_id, r_time=time.time()):
     msg = {
         "action": "release",
         "src": thread_id,
-        "tstamp": time.time()
+        "tstamp": r_time
     }
+    send_to_voting_set(thread_id, msg)
+
+
+def send_to_voting_set(thread_id, msg):
     try:
         gen = voting_set(thread_id)
         while 1:
-            voting_set_member = gen.next()
-            threads[voting_set_member][0].put(msg)
+            send_message(gen.next(), msg)
     except StopIteration:
         pass
 
@@ -205,8 +211,7 @@ def voting_set(me):
             yield x
     me = me - 1
     for x in range((me / n) * n + 1, (me / n) * n + n + 1):
-        if x != me + 1:
-            yield x
+        yield x
 
 
 if __name__ == "__main__":
